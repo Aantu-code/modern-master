f6c46dc0077fb9425392e8e0dd7550133713613d
Antu-code <jibonoshomapto3@gmail.com>
2022-03-05 13:39:41 -0800

    repo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+          mammoth:'2rem'
+      },
+      colors:{
+          primary:'#ff6363',
+          secondary:{
+            100: '#E2E2ED',
+            200:'#888883'
+          }
+      },
+      fontFamily:{
+        body:['Nunito']
+      }
+    },
   },
   plugins: [],
 }
